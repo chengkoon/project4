9729714463cbd564ca35deac596e041b69814c82
chengkoon <chengkoon@gmail.com>
2017-03-08 14:58:09 +0800

    first commit

---
+++ controllers/auth.js
@@ -0,0 +1,55 @@
+// at the very top, include the database models
+var db = require('../models');
+var passport = require('../config/ppConfig');
+var express = require('express');
+var router = express.Router();
+
+router.get('/signup', function(req, res) {
+  res.render('auth/signup');
+});
+
+router.post('/signup', function(req, res) {
+  db.user.findOrCreate({
+    where: { email: req.body.email },
+    defaults: {
+      name: req.body.name,
+      password: req.body.password
+    }
+  }).spread(function(user, created) {
+    if (created) {
+      // replace the contents of this if statement with the code below
+      passport.authenticate('local', {
+        successRedirect: '/',
+        successFlash: 'Account created and logged in'
+      })(req, res);
+    } else {
+      // console.log('Email already exists');
+      req.flash('error', 'Email already exists');
+      res.redirect('/auth/signup');
+    }
+  }).catch(function(error) {
+    // console.log('An error occurred: ', error.message);
+    req.flash('error', error.message);
+    res.redirect('/auth/signup');
+  });
+});
+
+router.get('/login', function(req, res) {
+  res.render('auth/login');
+});
+
+router.post('/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/auth/login',
+  failureFlash: 'Invalid username and/or password',
+  successFlash: 'You have logged in'
+}));
+
+router.get('/logout', function(req, res) {
+  req.logout();
+  // console.log('logged out');
+  req.flash('success', 'You have logged out');
+  res.redirect('/');
+});
+
+module.exports = router;
---
+++ index.js
@@ -0,0 +1,51 @@
+require('dotenv').config()
+var express = require('express');
+var session = require('express-session');
+var ejsLayouts = require('express-ejs-layouts');
+var bodyParser = require('body-parser');
+var passport = require('./config/ppConfig');
+var flash = require('connect-flash');
+var isLoggedIn = require('./middleware/isLoggedIn');
+var app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(require('morgan')('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(ejsLayouts);
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true
+}));
+// console.log(process.env.SESSION_SECRET);
+/*
+ * Include the flash module by calling it within app.use().
+ * IMPORTANT: This MUST go after the session module
+ */
+app.use(flash());
+
+// initialize the passport configuration and session as middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(function(req, res, next) {
+  // before every route, attach the flash messages and current user to res.locals
+  res.locals.currentUser = req.user;
+  res.locals.alerts = req.flash();
+  next();
+});
+
+app.get('/', function(req, res) {
+  res.render('index');
+});
+
+app.get('/profile', isLoggedIn, function(req, res) {
+  res.render('profile');
+});
+
+app.use('/auth', require('./controllers/auth'));
+
+var server = app.listen(process.env.PORT || 3000);
+
+module.exports = server;
